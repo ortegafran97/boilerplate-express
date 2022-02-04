ad9136b2d80493e7f527e52d85d035163def66de
Franco Ortega <31488989+ortegafran97@users.noreply.github.com>
2022-02-04 21:22:31 +0000

    Express challenges finished

--- myApp.js
+++ myApp.js
@@ -1,12 +1,47 @@
 var express = require('express');
+var bodyParser = require("body-parser")
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>{
-	res.send("Hello Express")
-	console.log("Hello Express")
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
 })
 
+app.get('/now', function(req, res, next) {
+	req.time = new Date().toString();
+	next();
+}, function(req, res) {
+	res.send({ time: req.time });
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+function HelloExpress(req, res) {
+	res.sendFile(__dirname + "/views/index.html")
+}
+
+app.get("/", HelloExpress)
+app.get("/json", (req, res) => {
+	let response = "Hello json";
+	if (process.env.MESSAGE_STYLE === "uppercase") { response = response.toUpperCase(); }
+	res.json({ "message": response })
+})
+
+app.get("/:word/echo", function(req, res) {
+	res.send({ echo: req.params.word })
+})
+
+app.route("/name")
+	.get(function(req, res) {
+		const response = `${req.query.first} ${req.query.last}`
+		res.json({ "name": response })
+	}).post(function(req, res) {
+		const response = `${req.body.first} ${req.body.last}`;
+		res.json({ "name": response })
+	}
+
+	)
 
 
 
@@ -35,10 +70,4 @@ app.get("/",(req,res)=>{
 
 
 
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
